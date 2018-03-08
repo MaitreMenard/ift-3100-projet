bf4ba2ec0db44e8852205b3378764e8975773e91
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-03-07 22:16:57 -0500

    UI has begun

--- harmony/src/ofApp.cpp
+++ harmony/src/ofApp.cpp
@@ -4,17 +4,65 @@
 
 void ofApp::setup()
 {
-    shiftIsPressed = false;
+	shiftIsPressed = false;
 
-    ofSetFrameRate(60);
-    ofEnableDepthTest();
+	ofSetFrameRate(60);
+	//ofEnableDepthTest();
 
-    camera.setNearClip(0.1f);
+	camera.setNearClip(0.1f);
 
-    scene.setup();
+	scene.setup();
 
-    Cube* cube = new Cube();
-    scene.addGameObject(cube);
+	Cube* cube = new Cube();
+	scene.addGameObject(cube);
+
+	ofSetVerticalSync(true);
+
+	// we add this listener before setting up so the initial circle resolution is correct
+	//circleResolution.addListener(this, &ofApp::circleResolutionChanged);
+
+	//gui.setup(); // most of the time you don't need a name
+	//gui.add(filled.setup("fill", true));
+	//gui.add(radius.setup("radius", 140, 10, 300));
+	//gui.add(center.setup("center", ofVec2f(ofGetWidth()*.5, ofGetHeight()*.5), ofVec2f(0, 0), ofVec2f(ofGetWidth(), ofGetHeight())));
+	//gui.add(color.setup("color", ofColor(100, 100, 140), ofColor(0, 0), ofColor(255, 255)));
+	//gui.add(circleResolution.setup("circle res", 5, 3, 90));
+	//gui.add(twoCircles.setup("two circles"));
+	//gui.add(textField.setup("textfield:", "type sth. here"));
+
+	gui.setup();
+	scale_label.setBackgroundColor(ofColor(80, 80, 80));
+	gui.add(scale_label.setup(ofParameter<string>("Scale")));
+	gui.add(scale_x.setup("X: ", 50, 0, 100));
+	gui.add(scale_y.setup("Y: ", 50, 0, 100));
+	gui.add(scale_z.setup("Z: ", 50, 0, 100));
+	position_label.setBackgroundColor(ofColor(80, 80, 80));
+	gui.add(position_label.setup(ofParameter<string>("Position")));
+	gui.add(position_x.setup("X: ", 0, FLT_MIN, FLT_MAX));
+	gui.add(position_y.setup("Y: ", 0, FLT_MIN, FLT_MAX));
+	gui.add(position_z.setup("Z: ", 0, FLT_MIN, FLT_MAX));
+	gui.add(rotation.setup("Rotation", ofVec3f(0, 0, 0), ofVec3f(0, 0, 0), ofVec3f(360, 360, 360)));
+	colorType.addListener(this, &ofApp::colorTypeChanged);
+	gui.add(colorType.setup("HSB", true));
+	gui.add(color.setup("Color", ofColor(100, 100, 140), ofColor(0, 0), ofColor(255, 255)));
+	gui.setPosition(ofGetWidth() - gui.getWidth() - 2, 2);
+
+	bHide = false;
+}
+
+void ofApp::exit()
+{}
+
+void ofApp::colorTypeChanged(bool & pressed)
+{
+	if (colorType.getName() == "RGB") {
+		colorType.setName("HSB");
+		//TODO: Change color into HSB
+	}
+	else {
+		colorType.setName("RGB");
+		//TODO: Change color into RGB
+	}
 }
 
 void ofApp::update()
@@ -26,11 +74,38 @@ void ofApp::draw()
 {
     ofClear(0);
 
+	ofBackgroundGradient(ofColor::white, ofColor::gray); //CHANGE LA COULEUR DU FOND D'ECRAN
+
     camera.setPosition(0, 0, 5);
 
     camera.begin();
     scene.draw();
     camera.end();
+
+	/*if (filled)
+	{
+		ofFill();
+	}
+	else
+	{
+		ofNoFill();
+	}*/
+
+	ofSetColor(color);
+	/*if (twoCircles)
+	{
+		ofDrawCircle(center->x - radius*.5, center->y, radius);
+		ofDrawCircle(center->x + radius*.5, center->y, radius);
+	}
+	else
+	{
+		ofDrawCircle((ofVec2f)center, radius);
+	}*/
+
+	if (!bHide)
+	{
+		gui.draw();
+	}
 }
 
 void ofApp::keyPressed(int key)
@@ -84,6 +159,9 @@ void ofApp::keyPressed(int key)
     case 2304:  // shift
         shiftIsPressed = true;
         break;
+	case 'h':
+		bHide = !bHide;
+		break;
     default:
         break;
     }
@@ -133,7 +211,7 @@ void ofApp::mouseExited(int x, int y)
 
 void ofApp::windowResized(int w, int h)
 {
-
+	
 }
 
 void ofApp::gotMessage(ofMessage msg)
--- harmony/src/ofApp.h
+++ harmony/src/ofApp.h
@@ -2,6 +2,8 @@
 #include "ofMain.h"
 #include "scene.h"
 #include "cube.h"
+#include "ofxGui.h"
+#include "ofxInputField.h"
 
 class ofApp : public ofBaseApp
 {
@@ -16,6 +18,8 @@ public:
     void update();
     void draw();
 
+	void exit();
+
     void keyPressed(int key);
     void keyReleased(int key);
     void mouseMoved(int x, int y);
@@ -27,4 +31,25 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
+
+	void colorTypeChanged(bool & pressed);
+
+	bool bHide;
+
+	ofxFloatField scale_x;
+	ofxFloatField scale_y;
+	ofxFloatField scale_z;
+
+	ofxFloatField position_x;
+	ofxFloatField position_y;
+	ofxFloatField position_z;
+
+	ofxColorSlider color;
+	ofxVec3Slider rotation;
+	ofxToggle colorType;
+
+	ofxPanel gui;
+
+	ofxLabel scale_label;
+	ofxLabel position_label;
 };
