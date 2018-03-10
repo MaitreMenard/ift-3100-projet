efbf4d2d2680e37a6319dee33802a666c257710c
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-03-10 13:25:38 -0500

    Replaced removeListener method with enable/disableEvents

--- harmony/src/gui/rotation_slider.h
+++ harmony/src/gui/rotation_slider.h
@@ -6,13 +6,17 @@
 class RotationSlider : public ofxVec3Slider
 {
 public:
+    RotationSlider();
     void addListener(std::function<void(ofVec3f)> method);
-    void removeListener(std::function<void(ofVec3f)> method);
+    void enableEvents();
+    void disableEvents();
     ofVec3f operator=(const ofVec3f & v);
     ~RotationSlider();
 
 private:
     std::vector<std::function<void(ofVec3f)>> listeners;
+    bool eventsEnabled;
 
     void slidersListener(float & value);
+    void callListenersWithSliderValues();
 };
