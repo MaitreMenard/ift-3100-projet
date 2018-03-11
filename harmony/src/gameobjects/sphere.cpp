e1f6f0b2afcf832cc9f1a01ecec5dbe27def9cda
Julien Becirovski <j.becirovski@gmail.com>
2018-03-11 08:45:49 -0400

    fix texture allocation when created

--- harmony/src/gameobjects/sphere.cpp
+++ harmony/src/gameobjects/sphere.cpp
@@ -5,6 +5,11 @@ Sphere::Sphere()
     ofSpherePrimitive spherePrimitive = ofSpherePrimitive();
     spherePrimitive.setRadius(defaultRadius);
 
+	ofPixels * pix = new ofPixels();
+	pix->allocate(1, 1, OF_PIXELS_RGB);
+	pix->setColor(0, 0, ofColor(255, 255, 255));
+	texture.allocate(*pix);
+
     model = spherePrimitive.getMesh();
     nbVertex = model.getVertices().size();
 }
