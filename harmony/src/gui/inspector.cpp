53fe77e7665442644042ccaedfcc1f2b0814accc
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-03-29 14:59:37 -0400

    WIP inspector

--- harmony/src/gui/inspector.cpp
+++ harmony/src/gui/inspector.cpp
@@ -7,6 +7,15 @@ Inspector::Inspector()
     panel.setHeaderBackgroundColor(headerColor);
 }
 
+void Inspector::update(GameObject* gameObject)
+{
+    positionFields = gameObject->getPosition();
+    rotation = gameObject->getEulerAngles();
+    scaleFields = gameObject->getScale();
+
+    colorPicker.setColor(gameObject->getColor());
+}
+
 void Inspector::draw()
 {
     panel.draw();
--- harmony/src/gui/inspector.h
+++ harmony/src/gui/inspector.h
@@ -1,15 +1,26 @@
 #pragma once
 #include "ofxGui.h"
+#include "gameobject.h"
+#include "gui/colorpicker.h"
+#include "gui/rotation_slider.h"
+#include "gui/vec3f_textfield.h"
 
 class Inspector
 {
 private:
     const string headerText = "Inspector";
     const ofColor headerColor = ofColor(24, 120, 24);
+
     ofxPanel panel;
+    Vec3fTextField positionFields;
+    RotationSlider rotation;
+    Vec3fTextField scaleFields;
+    ColorPicker colorPicker;
+    ofxIntField parent;
 
 public:
     Inspector();
+    void update(GameObject* gameObject);
 
     void draw();
 };
